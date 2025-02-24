3083bb2982a923045d0fa8407289e0bf9f8170ca
Maksym <maxpavliy@gmail.com>
2025-02-24 19:24:09 +0100

    packages error fixed

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(cors({
 app.use("/products/", shopRouter);
 app.use("/auth/", authRouter);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 //Connecting to database using mongoose
 mongoose.connect(uri)
